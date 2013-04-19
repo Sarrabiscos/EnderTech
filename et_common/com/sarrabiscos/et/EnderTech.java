3fab8f35ea53b64656cade7ae4f025da6207ad9d
Sarrabiscos <y.sarrabiscos@gmail.com>
2013-04-19 17:48:29 +0100

    Now with more smart code

--- et_common/com/sarrabiscos/et/EnderTech.java
+++ et_common/com/sarrabiscos/et/EnderTech.java
@@ -1,10 +1,15 @@
 package com.sarrabiscos.et;
 
 import com.sarrabiscos.et.item.Items;
+import com.sarrabiscos.et.lib.Reference;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 /** EnderTech
@@ -14,15 +19,22 @@ import cpw.mods.fml.common.network.NetworkMod;
  * @author sarrabiscos
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html) */
 
-@Mod(modid = EnderTech.modid, name = "EnderTech", version = "1.0")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class EnderTech {
 
-	public static final String modid = "Sarrabiscos_EnderTech";
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event) {
+		Items.init();
+	}
 
 	@Init
 	public void load(FMLInitializationEvent event) {
-		Items.init();
+
+	}
+
+	@PostInit
+	public void modsLoaded(FMLPostInitializationEvent event) {
 	}
 
 }
---
+++ et_common/com/sarrabiscos/et/lib/Reference.java
@@ -0,0 +1,9 @@
+package com.sarrabiscos.et.lib;
+
+public class Reference {
+
+	public static final String MOD_ID = "EnderTech";
+	public static final String MOD_NAME = "Ender Tech";
+	public static final String VERSION = "0.2";
+
+}
