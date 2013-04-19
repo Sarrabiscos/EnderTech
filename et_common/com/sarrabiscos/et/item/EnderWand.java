1674c4fb1c6a49b7528a38caa73f6875adbbb4e0
Sarrabiscos <y.sarrabiscos@gmail.com>
2013-04-19 17:54:01 +0100

    Minor changes

--- et_common/com/sarrabiscos/et/item/EnderWand.java
+++ et_common/com/sarrabiscos/et/item/EnderWand.java
@@ -1,15 +1,11 @@
 package com.sarrabiscos.et.item;
 
-import com.sarrabiscos.et.EnderTech;
-
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.item.EntityEnderPearl;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
@@ -20,17 +16,13 @@ import net.minecraft.world.World;
  * @author sarrabiscos
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html) */
 
-public class EnderWand extends Item {
+public class EnderWand extends ItemET {
 
 	public EnderWand(int id) {
 		super(id);
 		this.setCreativeTab(CreativeTabs.tabTools);
 	}
 
-	public void updateIcons(IconRegister par1IconRegister) {
-		this.iconIndex = par1IconRegister.registerIcon(EnderTech.modid + ":"
-				+ this.getUnlocalizedName());
-	}
 
 	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World,
 			EntityPlayer par3EntityPlayer) {
